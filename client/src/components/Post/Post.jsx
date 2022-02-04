3928c564adbc5f96a2005f0a2d60dd39142ec38e
Stephanie Clark <sc.aurora92@gmail.com>
2022-02-04 15:39:16 -0500

    deployed to heroku -issues with images, potential fix

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 import './post.css'
 
 export default function Post({post}) {
-    const PF = 'http://localhost:5000/images/'
+    const PF = 'https://localhost:5000/images/'
     return (
         <div className="post">
             {post.postPicture && (
